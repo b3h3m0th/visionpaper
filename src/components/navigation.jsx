1ecf271c065faddc2968a8d7cf84b574261ce559
Behemoth <simonostini@gmail.com>
2021-11-10 11:03:08 +0100

    a

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            Vision Paper ÜFA
+            Vision Paper
           </a>{' '}
         </div>
 
